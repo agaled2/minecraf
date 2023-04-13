d43ee2f885cf75f07f3b0c7f20c8a6b8f6a52c5f
agaled2 <130106607+agaled2@users.noreply.github.com>
2023-04-13 15:15:20 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 var Canvas = new fabric.Canvas("myCanvas");
 
 //Define el ancho inicial y el alto del cloque de imágenes 
@@ -15,17 +14,17 @@ var bloque = "";
 
 
 // Agrega una función llamada player_update() para agregar la imagen del jugador 
-function dibujarJugador(){
-    fabric.Image.fromURL("player.png", function(img){
+function dibujarJugador() {
+    fabric.Image.fromURL("player.png", function (img) {
         jugador = img
         jugador.scaleToWidth(150);
         jugador.scaleToHeight(140);
-        jugador.set({top:y , left:X})
+        jugador.set({ top: y, left: X })
         Canvas.add(jugador);
 
     })
 }
-window.addEventListener("keydown" , teclaPulsada);
+window.addEventListener("keydown", teclaPulsada);
 
 
 
@@ -33,26 +32,59 @@ window.addEventListener("keydown" , teclaPulsada);
 
 
 // Agrega una función llamada new_image() para agregar las diferentes imágene al presionar teclas específicas.
-function nuevoBloque(imagen){
-    fabric.Image.fromURL(imagen, function(img){
-        
+function nuevoBloque(imagen) {
+    fabric.Image.fromURL(imagen, function (img) {
+
         bloque = img
         bloque.scaleToWidth(medida);
         bloque.scaleToHeight(medida);
-        bloque.set({top:y , left:X})
-        Canvas.add( bloque);
+        bloque.set({ top: y, left: X })
+        Canvas.add(bloque);
     })
 }
 
-function teclaPulsada(e){
-    tecla = e.KeyCode
-    if(tecla == "39"){
+function teclaPulsada(e) {
+    tecla = e.keyCode
+    if (tecla == "39" && X < 855) {
         X = X + medida
+        Canvas.remove(jugador)
         dibujarJugador();
-        console.log("hola")
+
+
     }
-    if(tecla == "49"){ 
-nuevoBloque("ground.png")
+    if (tecla == "37" && X > 0) {
+        X = X - medida
+        Canvas.remove(jugador)
+        dibujarJugador();
     }
 
+    if (tecla == "40" && y < 600) {
+        y = y + medida
+        Canvas.remove(jugador)
+        dibujarJugador();
+    }
+    if (tecla == "38" && y > 0) {
+        y = y - medida
+        Canvas.remove(jugador)
+        dibujarJugador();
+    }
+
+    if (e.shiftKey &&tecla == "187") {
+        medida = medida + 10;
+        document.getElementById("ancho").innerHTML = medida
+        document.getElementById("alto").innerHTML = medida
+
+        
+    }
+    if (e.shiftKey &&tecla == "189") {
+        medida = medida - 10;
+        document.getElementById("ancho").innerHTML = medida
+        document.getElementById("alto").innerHTML = medida
+    }
+    if (tecla == "32") {
+        nuevoBloque("trunk.jpg")
+    }
+    if (tecla == "8") {
+        Canvas.remove(Canvas.getActiveObject())
+    }
 }
